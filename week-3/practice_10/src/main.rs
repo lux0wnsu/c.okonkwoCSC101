f4070a3a62db4972580efbdff29772ac6a278237
c.okonkwoCSC101 <francis.xoxo.10@gmail.com>
2024-11-02 23:53:20 +0100

    (My third commit)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let sum = 5550 + 7310;
+    println!("The sum of 5550 and 7310 is {}", sum);
+
+    let difference = 95.5 - 4.3;
+    println!("The difference of 95.5 and 4.3 is {}", difference);
+
+    let product:f32 = 4.0 * 30.0;
+    println!("The multiple of 4.0 and 30 is {}", product);
+
+    let quotient = 56.7 / 32.2;
+    println!("The division of 56.7 and 32.2 is {}", quotient);
+
+    let remainder = 43 % 5;
+    println!("The remainder of 43 and 5 = {}", remainder);
+}
---
+++ week-3/practice_6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\franc\OneDrive\Documents\c.okonkwoCSC101\week-3\practice-6\target\debug\deps\practice_6.exe: src/main.rs
+
+C:\Users\franc\OneDrive\Documents\c.okonkwoCSC101\week-3\practice-6\target\debug\deps\practice_6.d: src/main.rs
+
+src/main.rs:
